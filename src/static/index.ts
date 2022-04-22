1a50904d8a487ab68bf1774fb422a81db13000ce
Luciano <54756923+lucianovictor@users.noreply.github.com>
2022-04-22 14:13:06 -0300

    feat: configuring svg import

---
+++ src/static/index.ts
@@ -0,0 +1,31 @@
+import IconApple from './icons/apple.svg'
+import IconArrowDown from './icons/arrow-down.svg'
+import IconBattleNet from './icons/battle-net.svg'
+import IconBuy from './icons/buy.svg'
+import IconCheckGroup from './icons/check-group.svg'
+import IconSmartPhone from './icons/smart-phone.svg'
+import IconLink from './icons/link.svg'
+import IconNintendo from './icons/nintendo.svg'
+import IconPlay from './icons/play.svg'
+import IconPlaystation from './icons/playstation.svg'
+import IconPoints from './icons/points.svg'
+import IconDots from './icons/dots.svg'
+import IconUser from './icons/user.svg'
+import IconXbox from './icons/xbox.svg'
+
+export {
+  IconApple,
+  IconArrowDown,
+  IconBattleNet,
+  IconBuy,
+  IconCheckGroup,
+  IconSmartPhone,
+  IconLink,
+  IconNintendo,
+  IconPlay,
+  IconPlaystation,
+  IconPoints,
+  IconDots,
+  IconUser,
+  IconXbox
+}
